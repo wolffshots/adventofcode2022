6d574f21a9bb94c6cdf7b17016e202626787c281
jadon <16850875+wolffshots@users.noreply.github.com>
2022-12-03 16:13:16 +0200

    test: added test cases for day 1

--- day1/day1.go
+++ day1/day1.go
@@ -8,57 +8,74 @@ import (
 	"strings" // sub stringing and splitting
 )
 
+var max = []int{0, 0, 0}
+var maxElfIndex = []int{0, 0, 0}
+
+func CalculateElfCalories(elf string) int {
+	cals := strings.Split(elf, "\n")
+	current := 0
+	if cals[0] == "" {
+		return 0
+	}
+	// covert input calories and sum for the elf
+
+	for _, cal := range cals {
+		// convert string to int
+		cal, err := strconv.Atoi(cal)
+		if err != nil {
+			log.Fatalf("str conv failed with: %v", err)
+		}
+		current = current + cal // running sum for elf
+	}
+	return current
+}
+
+func InsertNewMax(elfIndex, newValue int) {
+	for maxIndex, currentMax := range max {
+		if newValue > currentMax {
+			// shift down current values
+			if maxIndex < len(max)-2 {
+				max[maxIndex+2] = max[maxIndex+1]
+				maxElfIndex[maxIndex+2] = maxElfIndex[maxIndex+1]
+			}
+			if maxIndex < len(max)-1 {
+				max[maxIndex+1] = max[maxIndex]
+				maxElfIndex[maxIndex+1] = maxElfIndex[maxIndex]
+			}
+			// insert new value
+			max[maxIndex] = newValue
+			maxElfIndex[maxIndex] = elfIndex
+			// break to prevent re-insertion
+			break
+        }
+	}
+}
+
 func main() {
 	data, err := os.ReadFile("data.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	elves := strings.Split(string(data), "\n\n")
-	var max = []int{0, 0, 0}
-	var maxElf = []int{0, 0, 0}
-	for elfIndex, elf := range elves {
-		cals := strings.Split(elf, "\n")
-		current := 0
-		// covert input calories and sum for the elf
-		for _, cal := range cals {
-			// convert string to int
-			cal, err := strconv.Atoi(cal)
-			if err != nil {
-				log.Fatalf("str conv failed with: %v", err)
-			}
-			current = current + cal // running sum for elf
-		}
-		fmt.Printf("Total for elf #%d: %d\n", elfIndex+1, current)
-		// check if we have a new max
-		for i, val := range max {
-			if current > val {
-				// shift down current values
-				if i < len(max)-2 {
-					max[i+2] = max[i+1]
-					maxElf[i+2] = maxElf[i+1]
-				}
-				if i < len(max)-1 {
-					max[i+1] = max[i]
-					maxElf[i+1] = maxElf[i]
-				}
-				// insert new value
-				max[i] = current
-				maxElf[i] = elfIndex
-				// break to prevent re-insertion
-				break
-			}
-		}
 
+	for elfIndex, elf := range elves {
+		newValue := CalculateElfCalories(elf)
+		fmt.Printf("Total for elf #%d: %d\n", elfIndex+1, newValue)
+		// check if we have a new max
+		InsertNewMax(elfIndex, newValue)
 	}
-	fmt.Println("Maxes:")
+    
+	fmt.Println("\nMaxes:")
 	topThree := 0
 	for i, val := range max {
-		fmt.Printf("Max[%d] for elf #%d: %d\n", i, maxElf[i]+1, val)
+		fmt.Printf("\tMax[%d] for elf #%d: %d\n", i, maxElfIndex[i]+1, val)
 		topThree = topThree + val
 	}
 
-	fmt.Printf("Top three are carrying: %d\n", topThree)
-	// 2022/12/01 15:03:04 Max[0] for elf #150: 71471
-	// 2022/12/01 15:03:04 Max[1] for elf #100: 70523
-	// 2022/12/01 15:03:04 Max[2] for elf #50: 68194
+	fmt.Printf("\n\tTop three are carrying: %d\n", topThree)
+	// Maxes:
+	// Max[0] for elf #150: 71471
+	// Max[1] for elf #100: 70523
+	// Max[2] for elf #119: 69195
+	// Top three are carrying: 211189
 }
---
+++ day1/day1_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert" // boolean assertions for pass and fail
+	"testing"                            // testing library
+)
+
+func TestCalculateElfCalories(t *testing.T) {
+	input := []string{
+		"8417\n8501\n5429\n2112\n6482\n7971\n9636\n4003", "",
+	}
+	want := []int{52551, 0}
+	for testIndex, testOutput := range want {
+		result := CalculateElfCalories(input[testIndex])
+		assert.Equal(t, testOutput, result)
+	}
+}
+
+func TestInsertNewMax(t *testing.T) {
+		input := []int{
+			0,
+            50624,
+            50623,
+            80624,
+            40624,
+            50624,
+            90655,
+		}
+
+        want := [][]int{
+            {0,0,0},
+            {50624,0,0},
+            {50624,50623,0},
+            {80624,50624,50623},
+            {80624,50624,50623},
+            {80624,50624,50624},
+            {90655,80624,50624},
+        }
+
+		for testIndex, testOutput := range want {
+            InsertNewMax(testIndex, input[testIndex])
+			assert.Equal(t, testOutput, max)
+		}
+}
